f881652c2baa19a4a1f2b2d65aff721b2851394e
Artem Bulakh <artem.bulakh1@gmail.com>
2021-03-08 22:01:21 +0300

    added request parsing

--- src/main.cpp
+++ src/main.cpp
@@ -7,35 +7,51 @@ using namespace boost::asio;
 using namespace nlohmann;
 using ip::tcp;
 
-std::string read_(tcp::socket & socket) {
+std::string read_(tcp::socket & socket)
+{
     streambuf buf;
     read_until(socket, buf, "\n");
     std::string data = buffer_cast<const char*>(buf.data());
     return data;
 }
 
-void send_(tcp::socket & socket, const std::string& message) {
+void send_(tcp::socket & socket, const std::string& message)
+{
     const std::string msg = message + "\n";
     write(socket, buffer(message));
 }
 
-int main() {
+void parse_message(std::string& message, json& http_request)
+{
+    const char *head = &message[0];
+    const char *tail = &message[0];
+
+    // Find request type
+    while (*tail != ' ') ++tail;
+    http_request["Type"] = std::string(head, tail);
+
+    // Find path
+    while (*tail == ' ') ++tail;
+    head = tail;
+    while (*tail != ' ') ++tail;
+    http_request["Path"] = std::string(head, tail);
+}
+
+int main()
+{
     io_service io_service;
     tcp::acceptor acceptor_(io_service, tcp::endpoint(tcp::v4(), 8000 ));
     tcp::socket socket_(io_service);
 
+    json http_request;
     while(true) {
         acceptor_.accept(socket_);
 
         std::string message = read_(socket_);
         std::cout << message << std::endl;
+        parse_message(message, http_request);
 
-        json j;
-        j["Response"] = "This is response";
-        j["JSON"] = "In JSON";
-        j["Yep"] = "YO";
-
-        send_(socket_, j.dump());
+        send_(socket_, http_request.dump());
 
         socket_.close();
     }
